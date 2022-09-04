c95b0cbc71086c4cc2e494f4396c9957ecdab546
Jed Jocson <jed.jocson@obf.ateneo.edu>
2022-09-04 21:00:00 +0800

    Add LoginActivity and RegisterActivity code. Add string constants.

--- app/src/main/java/com/avsecam/loginregistersharedprefs/LoginActivity.java
+++ app/src/main/java/com/avsecam/loginregistersharedprefs/LoginActivity.java
@@ -2,38 +2,66 @@ package com.avsecam.loginregistersharedprefs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.ViewById;
 
+
 @EActivity(R.layout.activity_login)
 public class LoginActivity extends AppCompatActivity {
+
     @ViewById(R.id.editTextUsername) EditText usernameField;
     @ViewById(R.id.editTextPassword) EditText passwordField;
     @ViewById(R.id.checkBoxRememberMe) CheckBox rememberMeCheckBox;
 
-    SharedPreferences sharedPreferences = getSharedPreferences("data", MODE_PRIVATE);
-    SharedPreferences.Editor editor = sharedPreferences.edit();
+    private SharedPreferences sharedPreferences = getSharedPreferences(getString(R.string.SHAREDPREFERENCES_NAME), MODE_PRIVATE);
+    private SharedPreferences.Editor editor = sharedPreferences.edit();
 
     @Click(R.id.buttonSignIn)
     public void onLoginButtonPressed() {
-
+        // Check if there are already saved credentials
+        if (checkForCredentials()) {
+            String savedUsername = sharedPreferences.getString(getString(R.string.USERNAME_KEY), "");
+            String savedPassword = sharedPreferences.getString(getString(R.string.PASSWORD_KEY), "");
+            String username = usernameField.getText().toString();
+            String password = passwordField.getText().toString();
+            // Compare credentials
+            boolean correctCredentials = username.equals(savedUsername) && password.equals(savedPassword);
+            if (correctCredentials) {
+                // Go to landing page
+            } else {
+                Toast.makeText(this, "Invalid credentials.", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(this, "Nothing saved.", Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Click(R.id.buttonRegister)
     public void onRegisterButtonPressed() {
-
+        Intent goToRegister = new Intent(this, RegisterActivity.class);
+        startActivity(goToRegister);
     }
 
     @Click(R.id.buttonClear)
     public void onClearButtonPressed() {
-        usernameField.setText("");
-        passwordField.setText("");
+        if (checkForCredentials()) {
+            editor.clear();
+            Toast.makeText(this, "Credentials cleared.", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "No credentials to clear.", Toast.LENGTH_SHORT).show();
+        }
+    }
+    
+    private boolean checkForCredentials() {
+        return sharedPreferences.contains(getString(R.string.USERNAME_KEY)) && sharedPreferences.contains(getString(R.string.PASSWORD_KEY));
     }
 }
--- app/src/main/java/com/avsecam/loginregistersharedprefs/RegisterActivity.java
+++ app/src/main/java/com/avsecam/loginregistersharedprefs/RegisterActivity.java
@@ -2,13 +2,47 @@ package com.avsecam.loginregistersharedprefs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import org.androidannotations.annotations.Click;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
+
+
+@EActivity(R.layout.activity_register)
 public class RegisterActivity extends AppCompatActivity {
+    @ViewById(R.id.editTextNewUsername) EditText usernameField;
+    @ViewById(R.id.editTextNewPassword) EditText passwordField;
+    @ViewById(R.id.editTextConfirmPassword) EditText confirmPasswordField;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+    private SharedPreferences sharedPreferences = getSharedPreferences(getString(R.string.SHAREDPREFERENCES_NAME), MODE_PRIVATE);
+    private SharedPreferences.Editor editor = sharedPreferences.edit();
+
+    @Click(R.id.buttonSave)
+    public void onSaveButtonPressed() {
+        String username = usernameField.getText().toString();
+        String password = passwordField.getText().toString();
+        String confirmPassword = confirmPasswordField.getText().toString();
+        // Check if all fields have values
+        if (username.length() > 0 && password.length() > 0 && confirmPassword.length() > 0) {
+            // Check if both passwords typed are equal
+            if (password.equals(confirmPassword)) {
+                editor.putString(getString(R.string.USERNAME_KEY), username);
+                editor.putString(getString(R.string.PASSWORD_KEY), password);
+                Toast.makeText(this, "Credentials saved.", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Passwords do not match!", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            String toastText = "";
+            if (username.length() <= 0) toastText += "Username must not be blank!\n";
+            if (password.length() <= 0) toastText += "Password must not be blank!\n";
+            toastText = toastText.substring(0, toastText.length() - 1);
+            Toast.makeText(this, toastText, Toast.LENGTH_SHORT).show();
+        }
     }
 }
